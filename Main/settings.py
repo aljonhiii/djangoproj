e03afa4cf7945c09e7786602c8951a545d5942d6
aljonhiii <ae202403715@wmsu.edu.ph>
2026-02-25 09:22:45 +0300

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hrdb',      
         'USER': 'root',
-        'PASSWORD': 'pogisialjon_26',      
+        'PASSWORD': '',      
         'HOST': '127.0.0.1',
         'PORT': '3307',
     }
